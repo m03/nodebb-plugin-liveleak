30d110987b0d84d0e77d0985870fe8b5e2920f76
m03 <moe.winnett@gmail.com>
2014-08-03 01:58:33 -0700

    Minor function refinement.

--- library.js
+++ library.js
@@ -1,20 +1,15 @@
 (function(module) {
   "use strict";
 
-  var basicUrl    = /<a href="(?:https?:\/\/)?(?:www\.)?liveleak\.(?:com|net|org)\/(?:e\/|view\?i=)([a-f0-9\-_]+)[&\/]?.*">.*<\/a>/gi;
-  var llembedUrl  = /<a href="(?:https?:\/\/)?(?:www\.)?liveleak\.(?:com|net|org)\/ll_embed\?f=([a-f0-9\-_]+)[&\/]?.*">.*<\/a>/gi;
-  var Liveleak    = {};
+  var basicUrl = /<a href="(?:https?:\/\/)?(?:www\.)?liveleak\.(?:com|net|org)\/(?:e\/|view\?i=)([a-f0-9\-_]+)[&\/]?.*">.*<\/a>/gi,
+      lleUrl = /<a href="(?:https?:\/\/)?(?:www\.)?liveleak\.(?:com|net|org)\/ll_embed\?f=([a-f0-9\-_]+)[&\/]?.*">.*<\/a>/gi,
+      basicEmbed = '<div class="video-container"><iframe class="liveleak-plugin" src="//www.liveleak.com/e/$1" allowfullscreen></iframe></div>',
+      llEmbed = '<div class="video-container"><iframe class="liveleak-plugin" src="//www.liveleak.com/ll_embed?f=$1" allowfullscreen></iframe></div>',
+      Liveleak = {};
   
-  Liveleak.parse  = function(postContent, callback) {
-    var embed     = '<div class="video-container"><iframe class="liveleak-plugin" src="//www.liveleak.com/e/$1" allowfullscreen></iframe></div>';
-
-    if (postContent.match(basicUrl)) {
-      postContent = postContent.replace(basicUrl, embed);
-    }
-    if (postContent.match(llembedUrl)) {
-      embed       = '<div class="video-container"><iframe class="liveleak-plugin" src="//www.liveleak.com/ll_embed?f=$1" allowfullscreen></iframe></div>';
-      postContent = postContent.replace(llembedUrl, embed);
-    }
+  Liveleak.parse = function(postContent, callback) {
+    postContent = postContent.replace(basicUrl, basicEmbed);
+    postContent = postContent.replace(lleUrl, llEmbed);
 
     callback(null, postContent);
   };
